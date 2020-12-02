35c45af1b920f6c96f33291dcf9bd91ab025121e
JorgenJorgensenSonOfJorgen <55201515+JorgenJorgensenSonOfJorgen@users.noreply.github.com>
2020-12-02 15:55:47 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 //Arithmetic Series Calculator
-document.getElementById("calculate").addEventListener("click", calculate)
 
-function calculate(){
+//global Variables
+let output = document.getElementById("output")
+//Calculate button click Event Listener
+document.getElementById("calculate").addEventListener("click", updateInput)
+
+function updateInput(){
     //inputs
     let d = Number(document.getElementById("d").value)
     let t1 = Number(document.getElementById("t1").value)
     let n = Number(document.getElementById("n").value)
-    let output = document.getElementById("output")
+    //output
+    output.innerHTML = calculate(d,t1,n)
+}
+
+function calculate(d,t1,n){
     //process
-    if (n < 0 || Math.round(n) == !n) {
-        output.innerHTML = "number of terms must be whole number"
+    if (n < 0 || Math.round(n) != n) {
+        return output.innerHTML = "number of terms must be whole number"
     } else {
-        //output
-        output.innerHTML = n/2*(2*t1 + d*(n-1))
+        return output.innerHTML = n/2*(2*t1 + d*(n-1))
     }
 }
